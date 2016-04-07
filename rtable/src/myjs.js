4f8d9826b9c8985acadf87410914181669351c25
Roman2702 <r_pugachoff@inbox.ru>
2016-04-07 19:20:15 +0300

    add sheets

--- rtable/src/myjs.js
+++ rtable/src/myjs.js
@@ -1,11 +1,26 @@
-var numStr = 30, numCol = 25; 
-function createTable(){
-//create a header
-var myElement = document.getElementById("tableHeader");
+var numStr = 30, numCol = 25, numSheets = 3; 
 var myTd, myTh, node, nameCol, myTr;
+
+function createTable(numSheet){
+//create a header
+var myElement = document.getElementById("myTable");
+//create a table
+var myTb = document.createElement("table");
+myTb.setAttribute("id",("sheet"+numSheet));
+//hide all sheets except 1st
+if (numSheet === 1) {myTb.setAttribute("class","visible");
+} else {myTb.setAttribute("class","hidden");}
+
 myTr = document.createElement("tr");
-nameCol = "Sheet";
-for (var i=1; i <= numCol; i++) {
+//create 1st cell
+myTh = document.createElement("th");
+nameCol = "Sheet" + numSheet;
+node = document.createTextNode(nameCol);
+myTh.appendChild(node);
+myTr.appendChild(myTh);
+
+//create raw of th 
+for (var i=1; i < numCol; i++) {
     myTh = document.createElement("th");
     if (i<27) {nameCol = getChar(i-1);}
     else if (i<53) {nameCol = 'A' + getChar(i-27);}
@@ -14,10 +29,11 @@ node = document.createTextNode(nameCol);
 myTh.appendChild(node);
 myTr.appendChild(myTh);
     }
-myElement.appendChild(myTr);
+myTb.appendChild(myTr);
+myElement.appendChild(myTb);
 
-//create e table
-myElement = document.getElementById("mainTable");
+
+
 for (var i = 0; i < numStr; i++) {
     myTr = document.createElement("tr");
     myElement.appendChild(myTr);
@@ -25,7 +41,7 @@ for (var i = 0; i < numStr; i++) {
     node = document.createTextNode(i+1);
     myTd.appendChild(node);
     myTr.appendChild(myTd);
-    for (var j = 0; j < numCol; j++) {
+    for (var j = 1; j < numCol; j++) {
         myTd = document.createElement("td");
         //myInput = document.createElement("input");
         myTd.setAttribute("id",getChar(j)+(i+1));//set id for td
@@ -34,11 +50,42 @@ for (var i = 0; i < numStr; i++) {
         //myTd.appendChild(myInput);
         myTr.appendChild(myTd);
     }
+    myTb.appendChild(myTr);
 }
+myElement.appendChild(myTb);
 }
 //list of char A - Z
-    function getChar(i){       
-        var startChar = "A";
-            endChar = "Z";
-            chCount = endChar.charCodeAt(0) - startChar.charCodeAt(0) + 1;
-        return String.fromCharCode(startChar.charCodeAt(0) + i) }
+function getChar(i){       
+    var startChar = "A";
+        endChar = "Z";
+        chCount = endChar.charCodeAt(0) - startChar.charCodeAt(0) + 1;
+    return String.fromCharCode(startChar.charCodeAt(0) + i) 
+}
+
+
+//create sheets
+function createSheets(numSheets){
+for (var i = 1; i <= numSheets; i++) {
+    createTable(i);
+}
+}
+
+//change vibility for all sheets ecsept numActiveSheet
+function setVisible(numActiveSheet){
+for (var i = 0; i < numSheets; i++) {
+    if (i === (numActiveSheet-1)) {
+        document.getElementsByTagName("table")[i].setAttribute("class", "visible");}
+        else {document.getElementsByTagName("table")[i].setAttribute("class", "hidden");}
+}
+}
+//add new sheet
+function addSheet(){
+createTable(++numSheets);
+myElement = document.getElementById("sheetSwitch");
+var myBut = document.createElement("button");
+myBut.setAttribute("onclick", "setVisible(numSheets)");
+node = document.createTextNode("sheet" + numSheets);
+myBut.appendChild(node);
+myElement.appendChild(myBut);
+
+}
