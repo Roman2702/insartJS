16f8f9b3d642be43c81f421fc3a8b1586a89a885
Roman2702 <r_pugachoff@inbox.ru>
2016-04-12 09:53:19 +0300

    Fixed: add new row function

--- rtable/src/myjs.js
+++ rtable/src/myjs.js
@@ -2,10 +2,10 @@
 var numRow = 100, numCol = 100, numSheets = 3, maxNumCol = 26*26*26; 
 var rowNum, cellNum;//coordinates of cell (still dosn't work)
 var myTd, myTh, node, nameCol, myTr;
-
+var numLetter = 26, numLetterSqr = numLetter * numLetter;//quantity of letters ('A'.. 'Z')
 function createTable(numSheet){
-var myInput;
-var myElement = document.getElementById("myTable");
+    var myInput;
+    var myElement = document.getElementById("myTable");
 //create a table in div id = myTable
 var myTb = document.createElement("table");
 var nameSheet = "sheet"+numSheet;
@@ -29,7 +29,7 @@ for (var i=0; i < numCol; i++) {
     node = document.createTextNode(nameCol);
     myTh.appendChild(node);
     myTr.appendChild(myTh);
-    }
+}
 myTb.appendChild(myTr);
 myElement.appendChild(myTb);
 
@@ -63,8 +63,8 @@ for (var i = 0; i < numRow; i++) {
                         var keyValue = e.target.getAttribute("id");
                         //myInput.parentNode.getAttribute("id"); 
                         if (myInput.value) {
-                        localStorage.setItem(keyValue, myInput.value);
-                    }
+                            localStorage.setItem(keyValue, myInput.value);
+                        }
                         //kill <input/> 
                         myInput.remove();
                         e.target.innerHTML = 
@@ -89,22 +89,23 @@ myElement.appendChild(myTb);
 //list of char A - Z
 function getChar(i){       
     var startChar = "A";
-        endChar = "Z";
-        chCount = endChar.charCodeAt(0) - startChar.charCodeAt(0) + 1;
+    endChar = "Z";
+    chCount = endChar.charCodeAt(0) - startChar.charCodeAt(0) + 1;
     return String.fromCharCode(startChar.charCodeAt(0) + i) 
 }
 function setNameCol(i){
-    if (i<26) {return getChar(i);}
-    else if (i<676){return getChar(Math.floor(i/26)-1)+getChar(i%26);}
-    else {var x1 = Math.floor(i/676);
-          var x2 = Math.floor((i%676)/26);
-          var x3 = i - x1*676 - x2*26;
-        return getChar(x1-1)+
-        getChar(x2)+getChar(x3);}
-}
+    if (i<numLetter) {return getChar(i);}
+    else if (i<numLetterSqr){return getChar(Math.floor(i/numLetter)-1)+
+        getChar(i%numLetter);}
+    else {var x1 = Math.floor(i/numLetterSqr);
+      var x2 = Math.floor((i%numLetterSqr)/numLetter);
+      var x3 = i - x1*numLetterSqr - x2*numLetter;
+      return getChar(x1-1)+
+      getChar(x2)+getChar(x3);}
+  }
 
-function setData(){
-this.getAttribute("id");
+  function setData(){
+    this.getAttribute("id");
 // Сохранение значения
 //localStorage.setItem("", "Значение")
 // Получение значения
@@ -113,28 +114,28 @@ this.getAttribute("id");
 
 //create sheets
 function createSheets(numSheets){
-for (var i = 1; i <= numSheets; i++) {
-    createTable(i);
-}
+    for (var i = 1; i <= numSheets; i++) {
+        createTable(i);
+    }
 }
 
 //change vibility for all sheets ecsept numActiveSheet
 function setVisible(numActiveSheet){
-for (var i = 0; i < numSheets; i++) {
-    if (i === (numActiveSheet-1)) {
-        document.getElementsByTagName("table")[i].setAttribute("class", "visible");}
-        else {document.getElementsByTagName("table")[i].setAttribute("class", "hidden");}
-}
-}
+    for (var i = 0; i < numSheets; i++) {
+        if (i === (numActiveSheet-1)) {
+            document.getElementsByTagName("table")[i].setAttribute("class", "visible");}
+            else {document.getElementsByTagName("table")[i].setAttribute("class", "hidden");}
+        }
+    }
 //add new sheet
 function addSheet(){
-createTable(++numSheets);
-myElement = document.getElementById("sheetSwitch");
-var myBut = document.createElement("button");
-myBut.setAttribute("onclick", "setVisible(numSheets)");
-node = document.createTextNode("sheet" + numSheets);
-myBut.appendChild(node);
-myElement.appendChild(myBut);
+    createTable(++numSheets);
+    myElement = document.getElementById("sheetSwitch");
+    var myBut = document.createElement("button");
+    myBut.setAttribute("onclick", "setVisible(numSheets)");
+    node = document.createTextNode("sheet" + numSheets);
+    myBut.appendChild(node);
+    myElement.appendChild(myBut);
 }
 function addRow(){
     // Find a <table> element with class="visible":
@@ -148,5 +149,36 @@ function addRow(){
     cell.appendChild(node);
     for (var i = 1; i <= numCol; i++) {
         var cell = row.insertCell(i);
+        cell.setAttribute("class","notIndex");
+        cell.addEventListener("dblclick", 
+            function(e){
+                if (e.target.innerHTML){
+                    var oldValue = e.target.innerHTML;
+                    e.target.innerHTML="";
+                } else {var oldValue ="";}
+                myInput = document.createElement("input");
+                myInput.value = oldValue;
+                myInput.addEventListener("blur", 
+                    function(){
+                        //write ti localStorage as "id of td" = "value of input"
+                        var keyValue = e.target.getAttribute("id");
+                        //myInput.parentNode.getAttribute("id"); 
+                        if (myInput.value) {
+                            localStorage.setItem(keyValue, myInput.value);
+                        }
+                        //kill <input/> 
+                        myInput.remove();
+                        e.target.innerHTML = 
+                        //document.getElementById(keyValue).innerHTML = 
+                        localStorage.getItem(keyValue);
+                    });
+                e.target.appendChild(myInput);
+                //var cellID = this.getAttribute("id");
+                //var myF = function(i,j){rowNum = i; cellNum = j;
+                //console.log("rowNum = " + rowNum + "  cellNum = "+ cellNum);};
+                
+                e.target.childNodes[0].focus();
+                
+            });
     }
 }
